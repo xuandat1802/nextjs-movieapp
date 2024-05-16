e258eab8b0e5c29aa9d6d26aa14de54a3b89715e
xuandat1802 <kaitou2k2@gmail.com>
2024-05-16 16:16:12 +0700

    add header

---
+++ src/components/Header.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Menuitem from './Menuitem';
+import { AiFillHome } from 'react-icons/ai';
+import { BsInfoCircleFill } from 'react-icons/bs';
+import Link from 'next/link';
+export default function Header() {
+  return (
+    <div className='flex justify-between items-center p-3 max-w-6xl mx-auto'>
+      <div className='flex gap-4'>
+        <Menuitem title='home' adress='/' Icon={AiFillHome}/>
+        <Menuitem title='about' adress='/about' Icon={BsInfoCircleFill}/>
+      </div>
+
+
+
+      <Link href={"/"} className='flex gap-1 items-center'>
+        <span className='bg-amber-500 font-bold rounded-lg py-1 px-2 text-2xl'>
+            IMDB
+        </span>
+        <span className='text-xl hidden sm:inline '>
+            Clone
+        </span>
+      </Link>
+    </div>
+  )
+}
+
---
+++ src/components/Menuitem.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Link from 'next/link'
+export default function Menuitem({title,adress,Icon}:any) {
+  return (
+      <Link className='hover:text-amber-400' href={adress}>
+        <Icon className='sm:hidden text-2xl'/>
+        <p className='text-2x hidden sm:inline text-sm uppercase'>{title}</p>  
+      </Link>
+  )
+}
