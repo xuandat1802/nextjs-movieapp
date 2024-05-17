fb43b86c21445fb1adb5f9876ae613b0d248faac
xuandat1802 <kaitou2k2@gmail.com>
2024-05-17 15:31:01 +0700

    add loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import Results from '@/components/Results';
 const API_KEY = process.env.API_KEY;
 export default async function Home({searchParams}:any) {
   const genre = searchParams.genre || 'fetchtrending';
-  const respone= await fetch(`https://api.themoviedb.org/3${genre==='fetchrarting'? `/movie/top_rated`:`/trending/all/week`}?api_key=${API_KEY}&language=en-US&page=1`);
+  const respone= await fetch(`https://api.themoviedb.org/3${genre==='fetchrarting'? `/movie/top_rated`:`/trending/all/week`}?api_key=${API_KEY}&language=en-US&page=1`,{next:{revalidate:100000}});
   const data=await respone.json();
   if(!respone.ok)
     {
